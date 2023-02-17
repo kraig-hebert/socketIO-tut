23c4bd07bf56dc47bbe566491f8b19e7710ac1b6
Kraig Hebert <kraighebert@gmail.com>
2023-02-16 19:50:31 -0500

    a client can now send a message to only other clients in the same room

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,18 @@ import io from 'socket.io-client';
 const socket = io.connect('http://localhost:3001');
 
 function App() {
+  // room state
+  const [room, setRoom] = useState('');
+
+  // messages states
   const [message, setMessage] = useState('');
   const [messageReceived, setMessageReceived] = useState('');
+
+  const joinRoom = () =>
+    room != '' ? socket.emit('join_room', room) : undefined;
+
   const sendMessage = () => {
-    socket.emit('send_message', { message });
+    socket.emit('send_message', { message, room });
   };
 
   useEffect(() => {
@@ -19,6 +27,13 @@ function App() {
   return (
     <div className="App">
       <input
+        type="text"
+        placeholder="Room Number"
+        onChange={(e) => setRoom(e.target.value)}
+      />
+      <button onClick={joinRoom}>Join Room</button>
+      <input
+        type="text"
         placeholder="Message..."
         onChange={(e) => {
           setMessage(e.target.value);
